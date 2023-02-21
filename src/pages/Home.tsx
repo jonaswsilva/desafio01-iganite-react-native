d724a193216caf314691df15b647bf56ab8707b8
Jonas Silva <jonaswdasilvaa@gmail.com>
2023-02-21 16:06:15 -0300

    desafio pronto 01

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,20 +5,42 @@ import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
+
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
     //TODO - add new task
+    const newTask = {
+      id: new Date().getDate(),
+      title: newTaskTitle,
+      done: false,
+    }
+
+    setTasks(oldState => [...tasks, newTask])
   }
 
   function handleToggleTaskDone(id: number) {
     //TODO - toggle task done if exists
+    const updateTasks = tasks.map(tasks => ({...tasks}))
+    
+    const findTask = updateTasks.find(item => item.id === id);
+
+    if(!findTask)
+      return
+
+    findTask.done = !findTask.done;
+    setTasks(updateTasks);
+
+
   }
 
   function handleRemoveTask(id: number) {
     //TODO - remove task from state
-  }
+    const updateTasks = tasks.filter(task => task.id !== id );
+
+    setTasks(updateTasks)
+    }
 
   return (
     <View style={styles.container}>
